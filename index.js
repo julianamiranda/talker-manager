6b02a52b0b384192fadd69ca9c3655f688cdb24d
Juliana Miranda <juliana.ms@live.com>
2022-07-12 14:37:26 -0300

    :white_check_mark: Requisito 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const { getTalker } = require('./utils/fs-talker');
+const msg = require('./utils/messages');
 
 app.use(bodyParser.json());
 
@@ -21,6 +22,18 @@ app.get('/talker', async (_req, res) => {
   if (!tk) return res.status(200).json([]);
   
   return res.status(200).json(tk);
+});
+
+// Crie o endpoint GET /talker/:id
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const tk = await getTalker();
+
+  const talkerById = tk.find((talker) => talker.id === Number(id));
+  
+  if (!talkerById) return res.status(404).json(msg.TALKER_NOT_FOUND);
+  
+  return res.status(200).json(talkerById);
 });
 
 app.listen(PORT, () => {
---
+++ utils/messages.js
@@ -0,0 +1,5 @@
+const TALKER_NOT_FOUND = { message: 'Pessoa palestrante não encontrada' };
+
+module.exports = {
+  TALKER_NOT_FOUND,
+};
